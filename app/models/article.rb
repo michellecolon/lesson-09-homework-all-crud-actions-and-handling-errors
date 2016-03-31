c1e31c9083a377ceec8e8a49e6eb71dd176634a2
Michelle Colon <michellecolon.mc@gmail.com>
2016-03-31 14:30:00 -0400

    HW 9- Corrections
    I added a modify column so that the edit/delete links would have
    somewhere to live.

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,5 @@ class Article < ActiveRecord::Base
   validates :title, uniqueness: { case_sensitive: false}
 
   validates :body, presence: true
-  validates :body, uniqueness: { case_sensitive: false}
+
 end
